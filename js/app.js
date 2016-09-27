4869feb67dbd8fc6f66d900bee8a79fd35e7aae2
Alexander Karpov <mrhx.me@gmail.com>
2016-09-27 17:42:22 +0300

    Added initial scroll support

--- js/app.js
+++ js/app.js
@@ -1,13 +1,24 @@
 (function (w, d) {
-    var widthEl = d.getElementById('width'), containerEl = d.getElementById('container');
-    var resizeFunc = function (event) {
+    var widthEl = d.getElementById('width'), containerEl = d.getElementById('container'), windowHeight = 960;
+    var resizeFunc = function () {
         var width = w.innerWidth || d.documentElement.clientWidth || d.body.clientWidth;
         if (width <= 0) {
             width = 960;
         }
         widthEl.value = width;
         containerEl.style.width = width + "px";
+        windowHeight = w.innerHeight || d.documentElement.clientHeight || d.body.clientHeight;
+    };
+    var scrollFunc = function () {
+        var top = d.body.scrollTop || d.documentElement.scrollTop;
+        if (top <= 0) {
+            console.log("top");
+        }
+        if (top >= windowHeight - 10) {
+            console.log("bottom");
+        }
     };
     w.onresize = resizeFunc;
+    w.onscroll = scrollFunc;
     resizeFunc();
 })(window, document);
